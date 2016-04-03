b3d34f84c6d82b4780c8511220a65bd0c7f64432
Jason Borowski <esc0rtd3w@gmail.com>
2016-04-03 18:10:34 -0400

    Added Source Scripting For Modules
    Added SuperAntiSpyware Batch Scripts

---
+++ pcl-win/data/modules/cleaning/scanners/sas/launch.cmd
@@ -0,0 +1,353 @@
+@echo off
+
+:reset
+
+color 0e
+
+::----------------------------------------------------------------------------------
+:: BEGIN SETTING VARIABLES
+::----------------------------------------------------------------------------------
+
+set scriptVersion=v2.1
+
+::----------------------------------------------------------------------------------
+:: THESE ARE THE ONLY VARIABLES NEEDED FOR GLOBAL COMPATIBILITY
+set appName=SuperAntiSpyware
+set appBin=SUPERAntiSpyware.exe
+set appVersion=6.0.1206
+:: Next Line Only Needed If All Users Folder Is Used
+set pathUpdatesLocalNameAU=SUPERAntiSpyware.com
+:: Next Line Only Needed If Current User Folder Is Used
+set pathUpdatesLocalNameCU=SUPERAntiSpyware.com
+:: Next Line Only Needed If AppData Local Folder Is Used
+::set pathUpdatesLocalNameADL=APPFOLDERNAMEHERE
+:: Next Line Only Needed If Temp AU Folder Is Used
+::set pathUpdatesLocalNameTempAU=APPFOLDERNAMEHERE
+:: Next Line Only Needed If Temp AU Folder Is Used
+::set pathUpdatesLocalNameTempCU=APPFOLDERNAMEHERE
+:: Next Line Only Needed If Checking For Local Installation
+::set pathUpdatesLocalNamePF=APPFOLDERNAMEHERE
+:: Next Line Only Needed For MSI Extraction
+::set msiExtractPathTemp=Program Files\APPFOLDERNAMEHERE
+
+:: Default Update Settings for use with WGET
+::set remoteFile=http://www.website.com/file.zip
+::set localFile=file.zip
+::set remoteFile64=http://www.website.com/file64.zip
+::set localFile64=file64.zip
+
+:: Used if only there are 2 separate x86 and x64 layouts
+set bitSplit=1
+::----------------------------------------------------------------------------------
+
+::----------------------------------------------------------------------------------
+:: Get CPU Architecture
+set bits=x86
+
+if not exist "C:\Program Files (x86)" set bits=x86
+if exist "C:\Program Files (x86)" set bits=x64
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" set bits=x64
+::----------------------------------------------------------------------------------
+
+::----------------------------------------------------------------------------------
+:: Check if user wants to use 32-bit version if 64-bit version is also available
+if %bitSplit%==1 (
+
+	if %bits%==x64 (
+		set forcex86=n
+		echo Do you want to use 32-bit Version? [Y/N] [Default: NO]:
+		echo.
+		echo.
+		set /p forcex86=
+	)
+	
+)
+
+if %forcex86%==Y set bits=x86
+if %forcex86%==y set bits=x86
+::----------------------------------------------------------------------------------
+
+::----------------------------------------------------------------------------------
+:: Check parent directory name (also sets module name)
+set moduleNameTemp=%~p0
+set moduleNameTemp=%moduleNameTemp:~1,-1%
+set moduleNameTemp=%moduleNameTemp:\=,%
+set moduleNameTemp=%moduleNameTemp: =_%
+for %%a in (%moduleNameTemp%) do set "moduleActive=%%a"
+set moduleActive=%moduleActive:_= %
+
+:: Setting working directories
+set extractedDir=%systemDrive%\PCLTEMP
+set pclTempDir=%temp%\pcl\temp
+
+:: If launched from PCL, global variables are not set and overwritten
+if "%pclActive%"=="" (
+set runDirectory=%cd%
+set pclActive=0
+)
+
+:: Setting global directories if not present
+set pclTempTemp=%temp%\pcl\temp
+set pclTempMain=%temp%\pcl
+
+:: Set update date and time
+set updateTimeDate=%date% %time%
+
+:: Common Paths
+::set pf=%systemdrive%\Program Files
+set pf=%ProgramFiles%
+set cf=%systemdrive%\Program Files\Common Files
+::set pf64=%systemdrive%\Program Files (x86)
+set pf64=%ProgramFiles(x86)%
+set cf64=%systemdrive%\Program Files (x86)\Common Files
+set pd=%ProgramData%
+set pdxp=C:\Documents and Settings\%username%\Application Data
+set adl=%LocalAppData%
+set adlxp=%USERPROFILE%\Local Settings\Application Data\Local
+set adll=%SystemDrive%%HomePath%\AppData\LocalLow
+set adllxp=%USERPROFILE%\Local Settings\Application Data\LocalLow
+set adr=%AppData%
+set adrxp=%USERPROFILE%\Local Settings\Application Data\Roaming
+set au=%ALLUSERSPROFILE%
+set auxp=C:\Documents and Settings\All Users
+set cu=%UserProfile%
+set win=%systemdrive%\Windows
+set sys32=%systemdrive%\Windows\system32
+
+:: Module Specific Items
+set pathCoreLocalRun=%runDirectory%\run
+if defined pathUpdatesLocalNameAU set pathUpdatesLocalAU=%au%\%pathUpdatesLocalNameAU%
+if defined pathUpdatesLocalNameCU set pathUpdatesLocalCU=%appdata%\%pathUpdatesLocalNameCU%
+if defined pathUpdatesLocalNameADL set pathUpdatesLocalADL=%adl%\%pathUpdatesLocalNameADL%
+if defined pathUpdatesLocalNameTempAU set pathUpdatesLocalTempAU=%windir%\temp\%pathUpdatesLocalNameTempAU%
+if defined pathUpdatesLocalNameTempCU set pathUpdatesLocalTempCU=%temp%\%pathUpdatesLocalNameTempCU%
+if defined pathUpdatesLocalNamePF set pathUpdatesLocalPF=%au%\%pathUpdatesLocalNamePF%
+
+if %pclActive%==0 (
+set pclExtract="%~dp0data\rar.exe" x -y
+set pclCompress="%~dp0data\rar.exe" a -m5
+set pclCompressBlob="%~dp0data\rar.exe" a -m5 -ep1
+set pack7z="%pathCoreLocalRun%\dependencies\7z.exe" a -y -r
+set packRar="%pathCoreLocalRun%\dependencies\rar.exe" a -m5
+set packZip="%pathCoreLocalRun%\dependencies\zip.exe"
+set unpack7z="%pathCoreLocalRun%\dependencies\7z.exe" -y x
+set unpackRar="%pathCoreLocalRun%\dependencies\rar.exe" x -y
+set unpackZip="%pathCoreLocalRun%\dependencies\unzip.exe"
+set sevenZ="%pathCoreLocalRun%\dependencies\7z.exe"
+set cocolor="%pathCoreLocalRun%\dependencies\cocolor.exe"
+set inifile="%pathCoreLocalRun%\dependencies\inifile.exe"
+set innoextract=%pathCoreLocalRun%\dependencies\innoextract.exe
+set extractInno=%pathCoreLocalRun%\dependencies\innoextract.exe
+set extractInnoSilent=%pathCoreLocalRun%\dependencies\innoextract.exe -q -s -d
+set instsrv="%pathCoreLocalRun%\dependencies\instsrv.exe"
+set md5sum="%pathCoreLocalRun%\dependencies\md5sums.exe" -b -e
+set msgbox="%pathCoreLocalRun%\dependencies\msgbox.exe"
+set msiexec=msiexec
+set msiExtract=msiexec /a
+set msiUninstall=msiexec /x
+set nircmd="%pathCoreLocalRun%\dependencies\nircmd.exe"
+set nop="%pathCoreLocalRun%\dependencies\nop.exe"
+set rar="%pathCoreLocalRun%\dependencies\rar.exe"
+set sfk="%pathCoreLocalRun%\dependencies\sfk.exe"
+set srvany="%pathCoreLocalRun%\dependencies\srvany.exe"
+set tail="%pathCoreLocalRun%\dependencies\tail.exe"
+set taskkill="%pathCoreLocalRun%\dependencies\taskkill.exe"
+set tasklist="%pathCoreLocalRun%\dependencies\tasklist.exe"
+set unzip="%pathCoreLocalRun%\dependencies\unzip.exe"
+set wait="%pathCoreLocalRun%\dependencies\wait.exe"
+set wget="%pathCoreLocalRun%\dependencies\wget.exe"
+set xml="%pathCoreLocalRun%\dependencies\xml.exe"
+set zip="%pathCoreLocalRun%\dependencies\zip.exe"
+)
+
+:: File Commands (Unix-Like)
+set cp=xcopy /e /y
+set cp2=xcopy /y
+set cp3=xcopy /e /y /h /d /c /r /i
+set cp4=xcopy /e /y /h /d /c /r /i /s
+set copy=copy /y
+set copyNoClobber=copy
+set del=del /f /q
+set rm=del /f /q
+set rmsubNoForce=del /s /q
+set rmsub=del /f /s /q
+set rmdir=rd /s /q
+set mkdir=md
+set kill=taskkill /f /im
+
+:: Default Scanner EXE
+set binScanner="%pathCoreLocalRun%\core\%appBin%"
+::set binArgs=
+set binUpdate="%pathCoreLocalRun%\core\%appBin%" /update
+set binUpdateSilent="%pathCoreLocalRun%\core\%appBin%" /update -silent
+
+:: Default Registry Files
+set regImportFromFile=reg import
+set regFileAdd="%pathCoreLocalRun%\registry\add.reg"
+set regFileRemove="%pathCoreLocalRun%\registry\remove.reg"
+set regFileRun="%pathCoreLocalRun%\registry\run.reg"
+set regFileSvcAdd="%pathCoreLocalRun%\registry\svc_add.reg"
+set regFileSvcAddSrvAny="%pathCoreLocalRun%\registry\svc_add_srvany.reg"
+set regFileSvcRemove="%pathCoreLocalRun%\registry\svc_remove.reg"
+
+:: Module Update Path For Copying New Updated Files Back To
+set pathUpdateBlobs=%runDirectory%\data\blobs
+
+:: Blob files running from default pclTempMain
+if %bitSplit%==0 (
+set blobCore="%pathUpdateBlobs%\core.blob"
+set blobDependencies="%pathUpdateBlobs%\dependencies.blob"
+if exist "%pathUpdateBlobs%\au.blob" set blobAllUsers="%pathUpdateBlobs%\au.blob"
+if exist "%pathUpdateBlobs%\cu.blob" set blobCurrentUser="%pathUpdateBlobs%\cu.blob"
+if exist "%pathUpdateBlobs%\adl.blob" set blobLocalAppData="%pathUpdateBlobs%\adl.blob"
+if exist "%pathUpdateBlobs%\tempau.blob" set blobTempAU="%pathUpdateBlobs%\tempau.blob"
+if exist "%pathUpdateBlobs%\tempcu.blob" set blobTempCU="%pathUpdateBlobs%\tempcu.blob"
+if exist "%pathUpdateBlobs%\misc.blob" set blobMisc="%pathUpdateBlobs%\misc.blob"
+if exist "%pathUpdateBlobs%\registry.blob" set blobRegistry="%pathUpdateBlobs%\registry.blob"
+)
+
+if %bitSplit%==1 (
+set blobCore="%pathUpdateBlobs%\core-%bits%.blob"
+set blobDependencies="%pathUpdateBlobs%\dependencies.blob"
+if exist "%pathUpdateBlobs%\au-%bits%.blob" set blobAllUsers="%pathUpdateBlobs%\au-%bits%.blob"
+if exist "%pathUpdateBlobs%\cu-%bits%.blob" set blobCurrentUser="%pathUpdateBlobs%\cu-%bits%.blob"
+if exist "%pathUpdateBlobs%\adl-%bits%.blob" set blobLocalAppData="%pathUpdateBlobs%\adl-%bits%.blob"
+if exist "%pathUpdateBlobs%\tempau-%bits%.blob" set blobTempAU="%pathUpdateBlobs%\tempau-%bits%.blob"
+if exist "%pathUpdateBlobs%\tempcu-%bits%.blob" set blobTempCU="%pathUpdateBlobs%\tempcu-%bits%.blob"
+if exist "%pathUpdateBlobs%\misc.blob" set blobMisc="%pathUpdateBlobs%\misc.blob"
+if exist "%pathUpdateBlobs%\registry.blob" set blobRegistry="%pathUpdateBlobs%\registry.blob"
+)
+
+:: Set default kill process list
+set killExe="%runDirectory%\data\kill.txt"
+
+:: The GUI creates this file upon launch
+set guiCheckFile="%windir%\temp\initGUI.txt"
+
+:: Set debugMode to 0 if not defined
+if not defined debugMode set debugMode=0
+
+::----------------------------------------------------------------------------------
+:: END SETTING VARIABLES - ALL VARIABLES IN USE SHOULD BE SET AT THIS POINT!!
+::----------------------------------------------------------------------------------
+
+:: Creating folder structure on local drive
+if not exist "%pathCoreLocalRun%" md "%pathCoreLocalRun%"
+if defined blobAllUsers md "%pathUpdatesLocalAU%"
+if defined blobCurrentUser md "%pathUpdatesLocalCU%"
+if defined blobLocalAppData md "%pathUpdatesLocalADL%"
+if defined blobTempAU md "%pathUpdatesLocalTempAU%"
+if defined blobTempCU md "%pathUpdatesLocalTempCU%"
+
+:: Extract blobs needed to execute normally to a temp run folder
+:: It is created inside the extracted module folder (%temp%\pcl\%moduleActive%\run\)
+:: This folder is removed upon scanning completion to prepare for repack if needed
+%pclExtract% %blobCore% "%pathCoreLocalRun%"
+%pclExtract% %blobRegistry% "%pathCoreLocalRun%"
+%pclExtract% %blobDependencies% "%pathCoreLocalRun%"
+if defined blobMisc %pclExtract% %blobMisc% "%pathCoreLocalRun%"
+if defined blobRegistry %pclExtract% %blobRegistry% "%pathCoreLocalRun%"
+
+:: Extract blobs for AllUsers, CurrentUser, and Temp Paths
+:: Only extract if needed for each module
+if defined blobAllUsers %pclExtract% %blobAllUsers% "%au%"
+if defined blobCurrentUser %pclExtract% %blobCurrentUser% "%appdata%"
+if defined blobLocalAppData %pclExtract% %blobLocalAppData% "%adl%"
+if defined blobTempAU %pclExtract% %blobTempAU% "%pathUpdatesLocalTempAU%"
+if defined blobTempCU %pclExtract% %blobTempCU% "%pathUpdatesLocalTempCU%"
+
+::----------------------------------------------------------------------------------
+:: ALL NEEDED FILES SHOULD BE AVAILABLE AT THIS POINT FOR LAUNCHING
+::----------------------------------------------------------------------------------
+
+if %debugMode%==1 pause
+
+:: Set window title
+title %appName% Portable Launcher %scriptVersion% :: portablecleaninglab.com
+
+:: Checking if running from PCL menu or standalone launch
+if "%pclActive%"=="" set runDirectory=%cd%
+
+:: Setting up registry settings
+%regImportFromFile% %regFileRun%
+
+:: Any services actions needed can be launched here
+:: Create a service
+::sc create "%serviceName%" binpath= "%runDirectory%\core\%appBin%" start= demand DisplayName= "%serviceDisplayName%" error= ignore
+
+:: Create a service (alternate)
+::instsrv.exe "%serviceName%" "%runDirectory%\dependencies\srvany.exe"
+
+:: Start a service
+::sc start "%serviceName%" binpath= "%runDirectory%\core\%appBin%"
+
+:: Stop a service
+::net stop %serviceName%
+
+:: Delete a service
+::sc delete "%serviceName%"
+
+:: Killing active processes
+setlocal enabledelayedexpansion
+set processList=0
+for /f "tokens=*" %%a in (%killExe%) do (
+    set /a processList=!processList! + 1
+	%kill% "%%a"
+)
+endlocal
+
+:: Launching App
+cls
+echo DO NOT CLOSE THIS WINDOW!!
+echo.
+echo.
+::echo Last Update: %updateTimeDate%
+echo.
+echo.
+if %bitSplit%==0 echo Launching %appName% Portable [%appVersion%]...
+if %bitSplit%==1 echo Launching %appName% Portable [%appVersion%] %bits%...
+echo.
+echo.
+
+:: Extract any last minute files (definitions, settings, etc)
+
+:: Using ZIP
+::%unpack7z% "%runDirectory%\data\blobs\FILE.ZIP" -o"%pathCoreLocalRun%"
+
+:: Using RAR
+::%unpackRar% %blobAllUsers% "%pathCoreLocalRun%"
+
+:: Launch main EXE file Standalone with No Arguments
+%binScanner%
+
+:: Launch main EXE file with Arguments
+::%binScanner% %binArgs%
+
+if %debugMode%==1 pause
+
+:: Killing active processes
+setlocal enabledelayedexpansion
+set processList=0
+for /f "tokens=*" %%a in (%killExe%) do (
+    set /a processList=!processList! + 1
+	%kill% "%%a"
+)
+endlocal
+
+:: Removing registry settings
+%regImportFromFile% %regFileRemove%
+
+:: Deleting traces of use
+%rmdir% %pathCoreLocalRun%
+if defined pathUpdatesLocalAU %rmdir% %pathUpdatesLocalAU%
+if defined pathUpdatesLocalCU %rmdir% %pathUpdatesLocalCU%
+if defined pathUpdatesLocalADL %rmdir% %pathUpdatesLocalADL%
+if defined pathUpdatesLocalTempAU %rmdir% %pathUpdatesLocalTempAU%
+if defined pathUpdatesLocalTempCU %rmdir% %pathUpdatesLocalTempCU%
+
+:: SuperAntiSpyware Specific
+%rm% "%SystemDrive%\SuperDELETE"
+
+
+:end
+::exit
